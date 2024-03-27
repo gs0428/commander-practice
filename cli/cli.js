a9bff5649b753a4a0be3760e23fb565b23ecd70a
gs0428 <lider28@naver.com>
2024-03-27 18:01:14 +0900

    feat: cli를 통해 가져온 컴포넌트를 폴더 생성 후 파일로 변환하는 작업

--- cli/cli.js
+++ cli/cli.js
@@ -1,3 +1,5 @@
+import { existsSync, promises as fs } from "fs";
+import path from "path";
 import { program } from "commander";
 import { getRegistry } from "./utils/registry.js";
 import { Command } from "commander";
@@ -7,13 +9,35 @@ const add = new Command()
   .description("add new component")
   .argument("[component]")
   .action(async (component) => {
+    const cwd = process.cwd();
+
     // 존재하는 컴포넌트라면 json 형식의 데이터를 반환
     // 존재하지 않는 컴포넌트라면 false를 반환
     const newComponent = await getRegistry(component);
 
     if (!newComponent) throw new Error(`There is no '${component}' component. Please check and retry.`);
 
-    const { type } = newComponent;
+    const { name, files, type } = newComponent;
+    const [parentFolder, childFolder] = type.split(":");
+    const targetDir = path.resolve(cwd, parentFolder, childFolder);
+
+    if (!existsSync(targetDir)) {
+      // targetDir이 존재하지 않는다면
+      // 상위 디렉토리와 함께 폴더 생성
+      // { recursive: true }가 상위 디렉토리를 생성 해줌
+      await fs.mkdir(targetDir, { recursive: true });
+    }
+
+    for (const file of files) {
+      const filePath = path.resolve(targetDir, file.name);
+
+      if (existsSync(filePath)) {
+        console.log(`${name} is component already exist.`);
+        continue;
+      }
+
+      await fs.writeFile(filePath, file.content);
+    }
   });
 
 program.addCommand(add).parse();
