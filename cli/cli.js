711e71638577a53dc27dd802caf93c2bfadf37ce
gs0428 <lider28@naver.com>
2024-04-01 13:11:27 +0900

    feat: all option을 이용한 모든 컴포넌트 가져오도록 구현

--- cli/cli.js
+++ cli/cli.js
@@ -1,63 +1,63 @@
 import { existsSync, promises as fs } from "fs";
 import path from "path";
 import { program, Command } from "commander";
-import { getRegistry } from "./utils/registry.js";
+import { getAllRegistries, getRegistry } from "./utils/registry.js";
 import { execa } from "execa";
 import ora from "ora";
+import { checkbox } from "@inquirer/prompts";
 
 const add = new Command()
   .name("add")
   .description("add new component")
-  .argument("<component>")
+  .argument("[components...]")
+  .option("-a, --all", "get all components", false)
   .option("-c, --cwd [cwd]", "chose directory where you want to install. default is current working directory.", process.cwd())
-  .action(async (component, option) => {
+  .action(async (components, option) => {
     const cwd = process.cwd();
 
     if (!existsSync(option.cwd)) throw new Error(`There is no '${cwd}'. Please check and try again.`);
 
-    // 존재하는 컴포넌트라면 json 형식의 데이터를 반환
-    // 존재하지 않는 컴포넌트라면 false를 반환
-    const newComponent = await getRegistry(component);
+    const allRegistries = await getAllRegistries();
 
-    if (!newComponent) throw new Error(`There is no '${component}' component. Please check and retry.`);
+    let selectedComponents = option.all ? allRegistries.map((registry) => registry.name) : components;
 
-    const spinner = ora(`Installing ${component}...`).start();
+    // const spinner = ora(`Installing ${component}...`).start();
 
-    const { name, dependencies, devDependencies, files, type } = newComponent;
-    const [parentFolder, childFolder] = type.split(":");
-    const targetDir = path.resolve(cwd, parentFolder, childFolder);
+    // const { name, dependencies, devDependencies, files, type } = newComponent;
+    // const [parentFolder, childFolder] = type.split(":");
+    // const targetDir = path.resolve(cwd, parentFolder, childFolder);
 
-    if (!existsSync(targetDir)) {
-      // targetDir이 존재하지 않는다면
-      // 상위 디렉토리와 함께 폴더 생성
-      // { recursive: true }가 상위 디렉토리를 생성 해줌
-      await fs.mkdir(targetDir, { recursive: true });
-    }
+    // if (!existsSync(targetDir)) {
+    //   // targetDir이 존재하지 않는다면
+    //   // 상위 디렉토리와 함께 폴더 생성
+    //   // { recursive: true }가 상위 디렉토리를 생성 해줌
+    //   await fs.mkdir(targetDir, { recursive: true });
+    // }
 
-    for (const file of files) {
-      const filePath = path.resolve(targetDir, file.name);
+    // for (const file of files) {
+    //   const filePath = path.resolve(targetDir, file.name);
 
-      if (existsSync(filePath)) {
-        console.log(`${name} is component already exist.`);
-        continue;
-      }
+    //   if (existsSync(filePath)) {
+    //     console.log(`\n${name} is component already exist.`);
+    //     continue;
+    //   }
 
-      await fs.writeFile(filePath, file.content);
-    }
+    //   await fs.writeFile(filePath, file.content);
+    // }
 
-    if (dependencies?.length) {
-      // execa 라이브러리를 이용하여
-      // 해당 컴포넌트의 의존성 설치
-      await execa("pnpm", ["add", ...dependencies], cwd);
-    }
+    // if (dependencies?.length) {
+    //   // execa 라이브러리를 이용하여
+    //   // 해당 컴포넌트의 의존성 설치
+    //   await execa("pnpm", ["add", ...dependencies], cwd);
+    // }
 
-    if (devDependencies?.length) {
-      // execa 라이브러리를 이용하여
-      // 해당 컴포넌트의 의존성 설치
-      await execa("pnpm", ["add", "-D", ...devDependencies], cwd);
-    }
+    // if (devDependencies?.length) {
+    //   // execa 라이브러리를 이용하여
+    //   // 해당 컴포넌트의 의존성 설치
+    //   await execa("pnpm", ["add", "-D", ...devDependencies], cwd);
+    // }
 
-    spinner.succeed("Done");
+    // spinner.succeed("Done");
   });
 
 program.addCommand(add).parse();
