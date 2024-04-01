e65e6d3feeb06ec476da1446fd9bcfe671ce44bc
gs0428 <lider28@naver.com>
2024-04-01 12:58:11 +0900

    feat: 모든 컴포넌트 가져오는 API 추가

--- cli/utils/registry.js
+++ cli/utils/registry.js
@@ -1,3 +1,8 @@
+export const getAllRegistries = async () => {
+  const response = await fetch("https://ui.shadcn.com/registry/index.json");
+  return response.json();
+};
+
 export const getRegistry = async (name) => {
   const response = await fetch(`https://ui.shadcn.com/registry/styles/default/${name}.json`);
   if (response.status === 404) return null;
